f4b16659e3ed2a285ca4235ad3203a08a1d86ea6
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 10:51:16 -0600

    adding more views and routes for practice

--- practiceEJS/app.js
+++ practiceEJS/app.js
@@ -4,6 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(require('./routes/index'));
+app.use(require('./routes/aboutus'));
+app.use(require('./routes/school'));
 
 
 app.listen(3004, () => {
