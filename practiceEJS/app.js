0247fb9c639918ab807cdbb98226e84d066fd1d9
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 11:10:04 -0600

    adding css

--- practiceEJS/app.js
+++ practiceEJS/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.static('public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/aboutus'));
 app.use(require('./routes/school'));
