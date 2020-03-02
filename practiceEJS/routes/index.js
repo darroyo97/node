00990fe8e959eb4c905fd2f20df7f97122c48ee1
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 11:18:31 -0600

    adding data from temp

--- practiceEJS/routes/index.js
+++ practiceEJS/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router()
 router.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index', {
+        pageTitle: 'Cooking with Mexicans',
+        description: 'tacos a la chingona '
+    })
 })
 module.exports = router;
