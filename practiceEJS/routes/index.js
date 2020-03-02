a5a87a8adc2f1a35ce055f117ab0d4e27d0ecdbb
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 11:28:01 -0600

    adding javascript with ejs

--- practiceEJS/routes/index.js
+++ practiceEJS/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router()
 router.get('/', (req, res) => {
+
+    let food = ['al pastor', 'fajita', 'carnitas', 'chicharron', 'pollo']
+
+
     res.render('index', {
         pageTitle: 'Cooking with Mexicans',
-        description: 'tacos a la chingona '
+        description: 'tacos a la chingona ',
+        foodList: food
     })
 })
 module.exports = router;
