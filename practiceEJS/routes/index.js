223797b9c439221c9658a8c9ad7bbc72aa565a2b
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 10:47:58 -0600

    using ejs views

--- practiceEJS/routes/index.js
+++ practiceEJS/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router()
 router.get('/', (req, res) => {
-    res.send('index page')
+    // res.send('index page')
+    res.render('index.ejs')
 })
 module.exports = router;
